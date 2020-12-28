06ac34c38e8e4534691ef89eb314fe9b235e88c4
cvlvxi <cvlvxi@outlook.com>
2020-12-29 09:45:22 +1100

    rejig stuff

--- learnmetal/learnmetal/Renderer.swift
+++ learnmetal/learnmetal/Renderer.swift
@@ -12,7 +12,6 @@ class Renderer: NSObject {
     static var commandQueue: MTLCommandQueue!
     var mesh: MTKMesh!
     var vertexBuffer: MTLBuffer!
-    var vertexBuffer2: MTLBuffer!
     var pipelineState: MTLRenderPipelineState!
     var timer: Float = 0
 
@@ -23,52 +22,42 @@ class Renderer: NSObject {
     ]
 
     init(metalView: MTKView) {
+        super.init()
         guard let device = MTLCreateSystemDefaultDevice() else {
             fatalError("GPU not available")
         }
         metalView.device = device
         Renderer.device = device
         Renderer.commandQueue = device.makeCommandQueue()!
-//
-//        let mdlMesh = Primitive.cube(device: device, size: 1.0)
-//        let mdlMesh = Primitive.sphere(device: device, size: 1.0)
-//        mesh = try! MTKMesh(mesh: mdlMesh, device: device)
-//        vertexBuffer = mesh.vertexBuffers[0].buffer
-        self.vertexBuffer2 = Renderer.device.makeBuffer(bytes: vertices, length: MemoryLayout<Vertex>.stride * vertices.count,  options: [])
+        
+        self.createVerticesBuffers()
+        self.createPipelineState(metalView)
        
+        metalView.clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1)
+        metalView.delegate = self
+    }
+    
 
-        let library = device.makeDefaultLibrary()
+    func createVerticesBuffers() {
+        self.vertexBuffer = Renderer.device.makeBuffer(bytes: vertices, length: MemoryLayout<Vertex>.stride * vertices.count,  options: [])
+    }
+    
+    func createPipelineState(_ metalView: MTKView) {
+        let library = Renderer.device.makeDefaultLibrary()
         let vertexFunction = library?.makeFunction(name: "vertex_main")
         let fragmentFunction = library?.makeFunction(name: "fragment_main")
 
         let pipelineDescriptor = MTLRenderPipelineDescriptor()
         pipelineDescriptor.vertexFunction = vertexFunction
         pipelineDescriptor.fragmentFunction = fragmentFunction
-//        pipelineDescriptor.vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(mdlMesh.vertexDescriptor)
-//        let vertexDescriptor = MTLVertexDescriptor()
-//        vertexDescriptor.attributes[0].format = .float3
-//        vertexDescriptor.attributes[0].bufferIndex = 0
-//        vertexDescriptor.attributes[0].offset = 0
-//        vertexDescriptor.attributes[1].format = .float4
-//        vertexDescriptor.attributes[1].bufferIndex = 0
-//        vertexDescriptor.attributes[1].offset = MemoryLayout<SIMD3<Float>>.stride
-//        vertexDescriptor.layouts[0].stride = MemoryLayout<SIMD3<Float>>.stride + MemoryLayout<SIMD4<Float>>.stride
-//        pipelineDescriptor.vertexDescriptor = vertexDescriptor
-//
-//
-        
         pipelineDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
-
+        
         // After doing setup stuff let's make render pipeline staate
         do {
-            pipelineState = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+            pipelineState = try Renderer.device.makeRenderPipelineState(descriptor: pipelineDescriptor)
         } catch let error {
             fatalError(error.localizedDescription)
         }
-
-        super.init()
-        metalView.clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1)
-        metalView.delegate = self
 
     }
 }
@@ -90,14 +79,8 @@ extension Renderer: MTKViewDelegate {
         renderEncoder.setVertexBytes(&currentTime, length: MemoryLayout<Float>.stride, index: 1)
 
         renderEncoder.setRenderPipelineState(pipelineState)
-        renderEncoder.setVertexBuffer(self.vertexBuffer2, offset: 0, index: 0)
-//        for submesh in mesh.submeshes {
-//            renderEncoder.drawIndexedPrimitives(type: .triangle,
-//                    indexCount: submesh.indexCount,
-//                    indexType: submesh.indexType,
-//                    indexBuffer: submesh.indexBuffer.buffer,
-//                    indexBufferOffset: submesh.indexBuffer.offset)
-//        }
+        renderEncoder.setVertexBuffer(self.vertexBuffer, offset: 0, index: 0)
+
         renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
 
         renderEncoder.endEncoding()
