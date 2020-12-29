4f4bd81312a993dd7202b4b3782821ef7f722bb3
cvlvxi <cvlvxi@outlook.com>
2020-12-29 13:56:37 +1100

    stuf

--- clitool/Tests/somelibTests/fsTests.swift
+++ clitool/Tests/somelibTests/fsTests.swift
@@ -3,11 +3,23 @@ import somelib
 import class Foundation.Bundle
 
 final class fsTests: XCTestCase {
+
+    var manager: FileManager = FileManager.default
+
+    override func setUp() {
+        if manager.currentDirectoryPath.contains(".build") {
+            manager.changeCurrentDirectoryPath("..")
+        }
+    }
+
+    func checkDirectoryExists(path: String) {
+        XCTAssertEqual(true, manager.fileExists(atPath: path))
+    }
+
+
     func testDataIterator() throws {
         let path = "data/mcap.txt"
-        let fm = FileManager.default
-        // Check tsv exists
-        XCTAssertEqual(true, fm.fileExists(atPath: path))
+        checkDirectoryExists(path: path)
         let fh = FileHandle(forReadingAtPath: path)
         let diter = DataIterator(fh, bufferSize: 1000)
         XCTAssertNotNil(diter)
@@ -17,6 +29,7 @@ final class fsTests: XCTestCase {
     
     func testLineProcessor() throws {
         let path = "data/mcap_small.txt"
+        checkDirectoryExists(path: path)
         let diter = DataIterator(FileHandle(forReadingAtPath: path), bufferSize: 100)
         var expectedStr: String = "#grch37_chrom\tpos\tref\talt\tmcapv1.4\tmcap_sensitivityv1.4"
         let lprocessor = LineProcessor(diter)
